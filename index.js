d9fad4370b40658f18ec591bba61eecfacf6c404
Tim Otten <totten@civicrm.org>
2018-06-01 15:24:14 -0700

    Move helper files under `lib/`

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = (robot) => {
   var path = require('path')
   var Mustache = require('mustache')
-  var vars = require('./variables')(robot, path.join(__dirname, 'repos'))
-  var templates = require('./templates')(robot, path.join(__dirname, 'repos'))
+  var vars = require('./lib/variables')(robot, path.join(__dirname, 'repos'))
+  var templates = require('./lib/templates')(robot, path.join(__dirname, 'repos'))
 
   robot.on('pull_request.opened', async context => {
     const template = await templates.find(context, 'PR_REPLY_TEMPLATE')
