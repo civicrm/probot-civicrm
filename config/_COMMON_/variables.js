7f28d89b858f15faaba1d0a32ebf914a32fb640c
Tim Otten <totten@civicrm.org>
2018-06-01 16:07:12 -0700

    Flatten config/_COMMON_/variables.js

--- config/_COMMON_/variables.js
+++ config/_COMMON_/variables.js
@@ -1,30 +1,30 @@
 module.exports = (context) => {
   const payload = context.payload
-  var ci = {
-    test_host: 'site-list.test-ubu1204-5.civicrm.org',
-    repo_prefix: function () {
-      const repo = payload.repository.full_name
-      const map = {
-        'exampleuser/examplerepo': 'ex',
-        'civicrm/civicrm-backdrop': 'bd',
-        'civicrm/civicrm-core': 'core',
-        'civicrm/civicrm-packages': 'pkg',
-        'civicrm/civicrm-drupal': 'd7',
-        'civicrm/civicrm-drupal-8': 'd8',
-        'civicrm/org.civicrm.flexmailer': 'flxm',
-        'totten/githubtest': 'ght'
-      }
-      return map[repo] ? map[repo] : null
-    },
-    browse_test_url: function () {
-      const prefix = ci.repo_prefix()
-      if (!prefix) {
-        return 'http://UNKNOWN-REPO'
-      }
-      return 'http://' + ci.test_host +
-        '/?filter=' + ci.repo_prefix() + '-' + payload.pull_request.number + '-*'
-    }
+  var tplVars = {ci: {}}
+
+  const prefixes = {
+    'exampleuser/examplerepo': 'ex',
+    'civicrm/civicrm-backdrop': 'bd',
+    'civicrm/civicrm-core': 'core',
+    'civicrm/civicrm-packages': 'pkg',
+    'civicrm/civicrm-drupal': 'd7',
+    'civicrm/civicrm-drupal-8': 'd8',
+    'civicrm/org.civicrm.flexmailer': 'flxm',
+    'totten/githubtest': 'ght'
   }
 
-  return {ci: ci}
+  tplVars.ci.test_host = 'site-list.test-ubu1204-5.civicrm.org'
+
+  tplVars.ci.browse_test_url = function () {
+    const repo = payload.repository.full_name
+    const prefix = prefixes[repo] ? prefixes[repo] : null
+    if (!prefix) {
+      return 'http://UNKNOWN-REPO'
+    }
+
+    return 'http://' + tplVars.ci.test_host +
+      '/?filter=' + prefix + '-' + payload.pull_request.number + '-*'
+  }
+
+  return tplVars
 }
