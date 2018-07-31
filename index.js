6c73cb6fb0f274315db7be5da4b8af618ac9c799
Tim Otten <totten@civicrm.org>
2018-07-31 16:00:07 -0700

    Move autoresponder logic from index.js to autoresponder-plugin.js

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
 module.exports = (robot) => {
-  var path = require('path')
-  var Mustache = require('mustache')
-  var vars = require('./lib/variables')(robot, path.join(__dirname, 'config'))
-  var templates = require('./lib/templates')(robot, path.join(__dirname, 'config'))
-
-  robot.on('pull_request.opened', async context => {
-    const template = await templates.find(context, 'PR_REPLY_TEMPLATE')
-    if (template === null || template === '') return
-    const body = Mustache.render(template, vars.generate(context))
-    return context.github.issues.createComment(context.issue({body: body}))
-  })
-
-  robot.on('issues.opened', async context => {
-    const template = await templates.find(context, 'ISSUE_REPLY_TEMPLATE')
-    if (template === null || template === '') return
-    const body = Mustache.render(template, vars.generate(context))
-    return context.github.issues.createComment(context.issue({body: body}))
-  })
+  require('./lib/autoresponder-plugin')(robot)
 }
