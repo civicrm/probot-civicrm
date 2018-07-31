19fc0d6f831eaaa1328c76d6a38cce83e369c883
Tim Otten <totten@civicrm.org>
2018-07-31 16:01:30 -0700

    test/*.js - Focus specifically on autoresponder

--- test/autoresponder/pr-opened.test.js
+++ test/autoresponder/pr-opened.test.js
@@ -1,8 +1,8 @@
 const {createRobot} = require('probot')
-const plugin = require('..')
-const payload = require('./fixtures/issue.opened')
+const plugin = require('../../lib/autoresponder-plugin')
+const payload = require('../fixtures/pull_request.opened')
 
-describe('probot-civicrm', () => {
+describe('probot-civicrm-autoresponder', () => {
   let robot
   let github
 
@@ -18,7 +18,7 @@ describe('probot-civicrm', () => {
         // Response for getting content from '.github/ISSUE_REPLY_TEMPLATE.md'
         getContent: jest.fn().mockImplementation(() => Promise.resolve({
           data: {
-            content: Buffer.from(`This is issue #{{issue.number}}. {{ci.foobar}}`).toString('base64')
+            content: Buffer.from(`[Browse test sites for #{{pull_request.number}} on {{ci.test_host}}]({{{ci.browse_test_url}}}/). {{ci.foobar}}`).toString('base64')
           }
         }))
       },
@@ -32,20 +32,20 @@ describe('probot-civicrm', () => {
     robot.auth = () => Promise.resolve(github)
   })
 
-  test('posts a comment on a new issue', async () => {
+  test('posts a comment on a new PR using repo template', async () => {
     await robot.receive(payload)
 
     expect(github.repos.getContent).toHaveBeenCalledWith({
       owner: 'exampleuser',
       repo: 'examplerepo',
-      path: '.github/ISSUE_REPLY_TEMPLATE.mustache.md'
+      path: '.github/PR_REPLY_TEMPLATE.mustache.md'
     })
 
     expect(github.issues.createComment).toHaveBeenCalledWith({
       owner: 'exampleuser',
       repo: 'examplerepo',
-      number: 7,
-      body: 'This is issue #7. whimsy'
+      number: 6,
+      body: '[Browse test sites for #6 on site-list.test-ubu1204-5.civicrm.org](http://site-list.test-ubu1204-5.civicrm.org/?filter=ex-6-*/). whimsy'
     })
   })
 })
