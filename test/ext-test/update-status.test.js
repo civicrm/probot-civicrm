44b8a8bec714c0906612f9f755b082b54cfa450b
Tim Otten <totten@civicrm.org>
2018-07-31 21:18:11 -0700

    ext-test - Split tests for pr-open and update-status

--- test/ext-test/update-status.test.js
+++ test/ext-test/update-status.test.js
@@ -3,7 +3,8 @@ process.env.STATUS_SECRET = 'tmp-signing-secret'
 process.env.STATUS_SHARED = 'tmp-auth-token'
 process.env.JENKINS_URL = 'https://user:apitoken@example.com:8080/jenkins'
 const plugin = require('../../lib/ext-test-plugin')
-const payload = require('../fixtures/pull_request.opened')
+const httpMocks = require('node-mocks-http')
+var jwt = require('jsonwebtoken')
 
 describe('probot-civicrm-ext-test', () => {
   let robot
@@ -36,26 +37,38 @@ describe('probot-civicrm-ext-test', () => {
   })
 
   test('marks the status as in-progress and fires async Jenkins job', async () => {
-    await robot.receive(payload)
-
-    expect(github.repos.getContent).toHaveBeenCalledWith({
-      owner: 'exampleuser',
-      repo: 'examplerepo',
-      path: 'info.xml'
+    const mockRequest = httpMocks.createRequest({
+      method: 'POST',
+      url: '/update-status',
+      query: {
+        state: 'success',
+        description: 'Fin',
+        statusToken: jwt.sign({
+          data: {
+            id: '1234', // context.id
+            insid: '5678', // context.payload.installation.id
+            tpl: {
+              owner: 'exampleuser', // context.repo().owner
+              repo: 'examplerepo', // context.repo().repo
+              sha: '74874d028346037875657ab0aeeaab222fabcfc7', // context.payload.pull_request.head.sha
+              context: 'CiviCRM Extension'
+            }
+          }
+        }, process.env.STATUS_SECRET, { expiresIn: '1d', algorithm: 'HS256' })
+      }
     })
+    const mockResponse = httpMocks.createResponse()
+
+    await require('../../lib/update-status-handler')(robot)(mockRequest, mockResponse)
+    expect(mockResponse._getData()).toBe('Accepted status update')
 
     expect(github.repos.createStatus).toHaveBeenCalledWith({
       owner: 'exampleuser',
       repo: 'examplerepo',
       sha: '74874d028346037875657ab0aeeaab222fabcfc7',
       context: 'CiviCRM Extension',
-      state: 'pending',
-      description: 'Waiting for the tests to complete'
-    })
-
-    expect(robot.jenkins.build_with_params).toHaveBeenCalledWith('Extension-PR', {
-      'PR_URL': 'https://github.com/exampleuser/examplerepo/pull/6',
-      'CIVI_VER': 'master'
+      state: 'success',
+      description: 'Fin'
     })
   })
 })
