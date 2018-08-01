062c921c37f0285a68ec54a431f1081f4870b868
Tim Otten <totten@civicrm.org>
2018-07-31 18:15:01 -0700

    ext-test-plugin - Fire Jenkins job

--- test/ext-test/pr-opened.test.js
+++ test/ext-test/pr-opened.test.js
@@ -1,6 +1,7 @@
 const {createRobot} = require('probot')
 process.env.STATUS_SECRET = 'tmp-signing-secret'
-process.env.STATUS_TOKEN = 'tmp-auth-token'
+process.env.STATUS_SHARED = 'tmp-auth-token'
+process.env.JENKINS_URL = 'https://user:apitoken@example.com:8080/jenkins'
 const plugin = require('../../lib/ext-test-plugin')
 const payload = require('../fixtures/pull_request.opened')
 
@@ -29,9 +30,12 @@ describe('probot-civicrm-ext-test', () => {
 
     // Mock out GitHub client
     robot.auth = () => Promise.resolve(github)
+    robot.jenkins = {
+      build_with_params: jest.fn()
+    }
   })
 
-  test('marks the status as in-progress', async () => {
+  test('marks the status as in-progress and fires Jenkins job', async () => {
     await robot.receive(payload)
 
     expect(github.repos.getContent).toHaveBeenCalledWith({
@@ -47,6 +51,20 @@ describe('probot-civicrm-ext-test', () => {
       context: 'CiviCRM Extension',
       state: 'pending',
       description: 'Waiting for the tests to complete'
+    })
+
+    expect(robot.jenkins.build_with_params).toHaveBeenCalledWith('Extension-PR', {
+      'PR_URL': 'https://github.com/exampleuser/examplerepo/pull/6',
+      'CIVI_VER': 'master'
+    })
+
+    expect(github.repos.createStatus).toHaveBeenCalledWith({
+      owner: 'exampleuser',
+      repo: 'examplerepo',
+      sha: '74874d028346037875657ab0aeeaab222fabcfc7',
+      context: 'CiviCRM Extension',
+      state: 'success',
+      description: 'Fin'
     })
 
     // TODO check that Jenkins is notified
